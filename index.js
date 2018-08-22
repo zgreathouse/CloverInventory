97c5c2a9ea1aa74dc7bd5239ea5c51e7e88a0f1f
zgreathouse <greathouse.zachary@gmail.com>
2018-08-22 13:32:23 -0700

    complete clover OAuth, build out frontend

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const keys = require('./config/keys');
 
 const app = express();
 
 //middlewares
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use(express.static('frontend'));
 
 //routes
 require('./routes/authRoutes')(app);
-require('./routes/inventoryRoutes')(app);
 
 //dynamic port binding
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5555;
 app.listen(PORT);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,29 @@
-const keys = require('../config/keys');
-let accessToken = "";
+const axios = require('axios');
+const keys = require('../keys');
+
+let accessCode = "";
+let apiToken = "";
 
 module.exports = app => {
   app.get('/', (req, res) => {
     if(req.query.code == undefined) {
-      res.redirect(301, `https://sandbox.dev.clover.com/oauth/authorize?client_id=${keys.APP_ID}&redirect_uri=${keys.REDIRECT_ID}`);
+      res.redirect(301,
+        `https://sandbox.dev.clover.com/oauth/authorize?client_id=${keys.APP_ID}`);
     } else {
-      accessToken = req.query.code;
+      accessCode = req.query.code;
+      requestAPIToken(res, req, accessCode);
     }
   });
 };
+
+//function which makes a request for an API token
+const requestAPIToken = (res, req, code) => {
+  const url = `https://sandbox.dev.clover.com/oauth/token?client_id=${keys.APP_ID}&client_secret=${keys.APP_SECRET}&code=${code}`
+
+  axios.get(url).then(response => {
+    apiToken = response.data;
+    res.send(apiToken);
+  }).catch(err => {
+    res.send({err});
+  });
+}
